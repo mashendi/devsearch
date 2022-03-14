733e01fdacd9a806978e72332617181037b2f724
Muhammed Shendi <39719317+mashendi@users.noreply.github.com>
2022-03-14 21:19:20 +0200

    added projects app

---
+++ projects/models.py
@@ -0,0 +1,18 @@
+from pyexpat import model
+import uuid
+from venv import create
+from django.db import models
+
+# Create your models here.
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True)
+
+    def __str__(self):
+        return self.title
---
+++ projects/urls.py
@@ -0,0 +1,9 @@
+from unicodedata import name
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.projects, name="projects"),
+    path("<str:pk>/", views.project, name="project")
+]
