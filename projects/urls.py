30dc680f6c7b305195267fd78ac0608e4e933a6a
Muhammed Shendi <39719317+mashendi@users.noreply.github.com>
2022-03-16 10:05:19 +0200

    added update and delete project

--- projects/urls.py
+++ projects/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     path("", views.projects, name="projects"),
     path("create-project/", views.createProject, name="create-project"),
+    path("update-project/<str:pk>/", views.updateProject, name="update-project"),
+    path("delete-project/<str:pk>/", views.deleteProject, name="delete-project"),
     path("<str:pk>/", views.project, name="project"),
 ]
