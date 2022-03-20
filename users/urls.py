2cdcfd41d7e3564817c3ca9163d45238b7ffb49a
Muhammed Shendi <39719317+mashendi@users.noreply.github.com>
2022-03-20 22:56:47 +0200

    profiles theme installed

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.profiles, name='profiles')
+    path('', views.profiles, name='profiles'),
+    path('<str:pk>', views.profile, name='profile')
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,16 @@
+from multiprocessing import context
 from django.shortcuts import render
-
+from .models import Profile
 # Create your views here.
+
+
 def profiles(request):
-    return render(request, 'users/profiles.html')
+    profiles = Profile.objects.all()
+    context = {'profiles': profiles}
+    return render(request, 'users/profiles.html', context)
+
+
+def profile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    context = {'profile': profile}
+    return render(request, 'users/profile.html', context)
