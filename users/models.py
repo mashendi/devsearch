5ce197443132f16468c56f5860f0b32a4529011c
Muhammed Shendi <39719317+mashendi@users.noreply.github.com>
2022-03-20 23:07:09 +0200

    Added model Skill

--- users/models.py
+++ users/models.py
@@ -26,3 +26,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user.username)
+
+
+class Skill(models.Model):
+    owner = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, blank=True, null=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
