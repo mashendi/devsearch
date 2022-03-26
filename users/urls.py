d05685e390400571171baf20c1b0235f9ab6ff0a
Muhammed Shendi <39719317+mashendi@users.noreply.github.com>
2022-03-26 20:35:15 +0200

    edit and delete skill"
    "

--- users/urls.py
+++ users/urls.py
@@ -2,14 +2,19 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    # auth urls
     path('login/', views.loginUser, name='login'),
     path('logout/', views.logoutUser, name='logout'),
     path('register/', views.registerUser, name='register'),
 
+    # profile and account urls
     path('', views.profiles, name='profiles'),
     path('profile/<str:pk>', views.profile, name='profile'),
     path('account/', views.account, name='account'),
     path('edit-profile/', views.editProfile, name='edit-profile'),
 
-    path('create-skill/', views.createSkill, name='create-skill')
+    # skill urls
+    path('create-skill/', views.createSkill, name='create-skill'),
+    path('edit-skill/<str:pk>', views.editSkill, name='edit-skill'),
+    path('delete-skill/<str:pk>', views.deleteSkill, name='delete-skill')
 ]
