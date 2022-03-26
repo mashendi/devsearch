a8fd5dbd709668ea382661688b5cb72dfa5bc9ee
Muhammed Shendi <39719317+mashendi@users.noreply.github.com>
2022-03-26 22:38:49 +0200

    search projects implemented

--- projects/views.py
+++ projects/views.py
@@ -3,11 +3,12 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Project
 from .forms import ProjectForm
+from .utils import searchProjects
 
 
 def projects(request):
-    projects = Project.objects.all()
-    context = {"projects": projects}
+    projects, search_query = searchProjects(request)
+    context = {"projects": projects, 'search_query': search_query}
     return render(request, 'projects/projects.html', context)
 
 
