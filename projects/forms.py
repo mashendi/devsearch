45a186eb579793e31cf57ef932146a9a02e0d9bd
Muhammed Shendi <39719317+mashendi@users.noreply.github.com>
2022-03-27 03:32:41 +0200

    added ReviewForm

--- projects/forms.py
+++ projects/forms.py
@@ -1,20 +1,36 @@
 from dataclasses import fields
 from django.forms import ModelForm
 from django import forms
-from .models import Project
+from .models import Project, Review
 
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ["title",  "description",
-                  "demo_link", "source_link", "tags", "featured_image",]
+                  "demo_link", "source_link", "tags", "featured_image", ]
         widgets = {
             "tags": forms.CheckboxSelectMultiple()
         }
 
     def __init__(self, *args, **kwargs):
         super(ProjectForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({"class": "input"})
+
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = ['value', 'body']
+        labels = {
+            'value': 'Your Vote',
+            'body': 'Write your feedback'
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ReviewForm, self).__init__(*args, **kwargs)
 
         for name, field in self.fields.items():
             field.widget.attrs.update({"class": "input"})
