fd5e44fe1fb5c18830ed69d4c4e186e902bcb51e
Muhammed Shendi <39719317+mashendi@users.noreply.github.com>
2022-03-29 23:19:56 +0200

    get projects and one project by id

--- api/views.py
+++ api/views.py
@@ -1,5 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import ProjectSerializer
+from projects.models import Project
+
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -13,3 +16,16 @@ def getRoutes(request):
     ]
     return Response(routes)
 
+
+@api_view(['GET'])
+def getProjects(request):
+    projects = Project.objects.all()
+    serializer = ProjectSerializer(projects, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getProject(request, pk):
+    project = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(project, many=False)
+    return Response(serializer.data)
